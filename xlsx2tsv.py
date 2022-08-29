d9b5244aba87014e200717235e9c2a9cfb3a980b
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2022-08-29 20:08:13 +0900

    文字化け手修正

--- xlsx2tsv.py
+++ xlsx2tsv.py
@@ -3,17 +3,24 @@ from csv import writer
 from openpyxl import load_workbook
 
 
-XLSX_OKI2YAMATO = "./okinawa_01.xlsx"
+XLSX_OKI2YAMATO = "./data/okinawa_01.xlsx"
+
+zen2han_dict = {chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}
+zen2han_dict.update({'、': ','})
+
 
 def main():
     wb = load_workbook(XLSX_OKI2YAMATO)
     def cells2values(row):
-        return [e.value for e in row]
+        new_row = [e.value for e in row]
+        new_row[1] = new_row[1].translate(str.maketrans(zen2han_dict))
+        return new_row
     rows = [cells2values(row) for row in list(wb.worksheets[0].rows)]
+    # rows = [[e.value for e in row] for row in list(wb.worksheets[0].rows)]
     file_name = XLSX_OKI2YAMATO.replace(".xlsx", ".tsv")
     with open(file_name, 'w') as fp:
         tsv_writer = writer(fp, delimiter="\t")
-        for row in rows:
+        for row in rows:            
             tsv_writer.writerow(row)
     
 
