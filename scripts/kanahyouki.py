3ca115fb1d9b9cb7ea31f43d10c948b04365eaa5
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2022-09-02 21:50:48 +0900

    pylint 導入

--- scripts/kanahyouki.py
+++ scripts/kanahyouki.py
@@ -1,16 +1,3 @@
-from typing import List, Tuple
-
-
-vowels = {'a', 'i', 'u', 'e', 'o'}
-consonants = {'C', 'S', 'Z', 'b', 'c', 'd', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'z'}
-semi_vowels = {'j', 'w'}
-sokuon = {'Q'}
-hatsuon = {'N'}
-glottal_stops = { "'", '?'}
-others ={' ', '(', ')', ',', '-', '=', ']'}
-
-exceptions = ["hNN"] # 発音記号の例外
-
 """
 日本語（沖縄語）の音節（モーラ？）のスキーム
 mora::=[g]([P]|[C][v]V[V])
@@ -21,69 +8,91 @@ word::=mora+...
 - v:semi-vowel
 - V:vowel
 """
+from typing import List, Tuple
+import json
+from itertools import product
+
+vowels = {'a', 'i', 'u', 'e', 'o'}
+consonants = {
+    'C', 'S', 'Z', 'b', 'c', 'd', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'q', 'r',
+    's', 't', 'z'
+}
+semi_vowels = {'j', 'w'}
+sokuon = {'Q'}
+hatsuon = {'N'}
+glottal_stops = {"'", '?'}
+others = {' ', '(', ')', ',', '-', '=', ']'}
+
+exceptions = ["hNN"]  # 発音記号の例外
 
 
-def delete_others(word: str) -> str:
+def _delete_others(pronunciation: str) -> str:
+    """発音記号の文字列から、子音、半母音、母音以外の文字を消去します。"""
     for other_chr in others:
-        word = word.replace(other_chr, '')
+        word = pronunciation.replace(other_chr, '')
     return word
 
 
 def _check_glottal_stop(ch_list: List[str]) -> Tuple[str, List[str]]:
-    ch = ch_list[0]
-    if ch in glottal_stops:
-        return _check_consonant(ch, ch_list[1:])
-    else:
-        return _check_consonant('', ch_list)
+    char = ch_list[0]
+    if char in glottal_stops:
+        return _check_consonant(char, ch_list[1:])
+    return _check_consonant('', ch_list)
 
 
-def _check_consonant(mora:str, ch_list: List[str]) -> Tuple[str, List[str]]:
-    ch = ch_list[0]
-    if ch in sokuon.union(hatsuon):
-        return ch, ch_list[1:]
-    elif ch in consonants:
-        return _check_semi_vowels(mora + ch, ch_list[1:])
-    else:
-        return _check_semi_vowels(mora, ch_list)
+def _check_consonant(mora: str, ch_list: List[str]) -> Tuple[str, List[str]]:
+    char = ch_list[0]
+    if char in sokuon.union(hatsuon):
+        return char, ch_list[1:]
+    if char in consonants:
+        return _check_semi_vowels(mora + char, ch_list[1:])
+    return _check_semi_vowels(mora, ch_list)
 
 
-def _check_semi_vowels(mora:str, ch_list: List[str]) -> Tuple[str, List[str]]:
-    ch = ch_list[0]
-    if ch in semi_vowels:
-        return _check_vowel(mora + ch, ch_list[1:])
-    else:
-        return _check_vowel(mora, ch_list)
-    
-
-def _check_vowel(mora:str, ch_list: List[str]) -> Tuple[str, List[str]]:
-    ch = ch_list[0]
-    if ch in vowels:
-        return _check_ending(mora + ch, ch_list[1:])
-    else:
-        raise Exception(f"{mora}の次は、母音{{aeiou}}が続きます。")
+def _check_semi_vowels(mora: str, ch_list: List[str]) -> Tuple[str, List[str]]:
+    char = ch_list[0]
+    if char in semi_vowels:
+        return _check_vowel(mora + char, ch_list[1:])
+    return _check_vowel(mora, ch_list)
 
 
-def _check_ending(mora:str, ch_list: List[str]) -> Tuple[str, List[str]]:
+def _check_vowel(mora: str, ch_list: List[str]) -> Tuple[str, List[str]]:
+    char = ch_list[0]
+    if char in vowels:
+        return _check_ending(mora + char, ch_list[1:])
+    raise Exception(f"{mora}の次は、母音{{aeiou}}が続きます。")
+
+
+def _check_ending(mora: str, ch_list: List[str]) -> Tuple[str, List[str]]:
     if len(ch_list) == 0:
         return mora, ch_list
-    ch = ch_list[0]
-    if ch in vowels and mora[-1] == ch:
-        return mora + ch, ch_list[1:]
-    else:
-        return mora, ch_list
+    char = ch_list[0]
+    if char in vowels and mora[-1] == char:
+        return mora + char, ch_list[1:]
+    return mora, ch_list
 
 
-def split_into_moras(word:str) -> List[str]:
-    if word in exceptions:
-        return [word]
-    word = delete_others(word)
+def split_into_moras(pronunciation: str) -> List[str]:
+    """発音記号の文字列をモーラに分解します。"""
+    if pronunciation in exceptions:
+        return [pronunciation]
+    word = _delete_others(pronunciation)
     chr_list = list(word)
-    moras : List[str] = []
-    while (len(chr_list)):
-        m, chr_list = _check_glottal_stop(chr_list)
-        moras.append(m)
-            
+    moras: List[str] = []
+    while chr_list:
+        mora, chr_list = _check_glottal_stop(chr_list)
+        moras.append(mora)
     return moras
 
-def convert2kana(pronunciation: str) -> str:
-    pass
+
+def convert2kana(pronunciation: str) -> List[str]:
+    """発音記号をかな表記に変換します。"""
+    with open("resources/kana-table.json", 'r') as kana_list_file:
+        pronunc_kana_dict = json.load(kana_list_file)
+    kana_list = []
+    for mora in split_into_moras(pronunciation):
+        kana_list.append(pronunc_kana_dict[mora])
+    converted = []
+    for kana in product(*kana_list):
+        converted.append("".join(kana))
+    return converted
--- scripts/xlsx2tsv.py
+++ scripts/xlsx2tsv.py
@@ -1,7 +1,10 @@
+"""
+国立国語研究所の沖縄語辞典のxlsx化されたデータを読み込んで、tsvファイルに変換します。
+"""
+
 from csv import writer
 
 from openpyxl import load_workbook
-
 
 XLSX_OKI2YAMATO = "./data/okinawa_01.xlsx"
 
@@ -10,19 +13,21 @@ zen2han_dict.update({'、': ','})
 
 
 def main():
-    wb = load_workbook(XLSX_OKI2YAMATO)
+    workbook = load_workbook(XLSX_OKI2YAMATO)
+
     def cells2values(row):
+        """xlsx形式の行を文字列の行に変換します。"""
         new_row = [e.value for e in row]
         new_row[1] = new_row[1].translate(str.maketrans(zen2han_dict))
         return new_row
-    rows = [cells2values(row) for row in list(wb.worksheets[0].rows)]
-    # rows = [[e.value for e in row] for row in list(wb.worksheets[0].rows)]
+
+    rows = [cells2values(row) for row in list(workbook.worksheets[0].rows)]
     file_name = XLSX_OKI2YAMATO.replace(".xlsx", ".tsv")
-    with open(file_name, 'w') as fp:
-        tsv_writer = writer(fp, delimiter="\t")
-        for row in rows:            
+    with open(file_name, 'w') as tsv_file:
+        tsv_writer = writer(tsv_file, delimiter="\t")
+        for row in rows:
             tsv_writer.writerow(row)
-    
+
 
 if __name__ == '__main__':
     main()
