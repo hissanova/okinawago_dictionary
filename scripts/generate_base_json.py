b6a40c98c7a6d74a66b9832392a2698f3cc8332c
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2022-09-17 19:38:09 +0900

    日沖辞典json作成

--- scripts/generate_base_json.py
+++ scripts/generate_base_json.py
@@ -1,36 +1,67 @@
+import argparse
 import json
 from csv import DictReader
+from pathlib import Path
 
 from kanahyouki import convert2kana
 
 
-def convert(tsv_row):
-    res = {}
-    res["page-in-dict"] = tsv_row["辞書\nページ"]
-    pronuc = tsv_row["見出し語"]
-    res["pronunciation"] = pronuc
-    res["index"] = convert2kana(pronuc)
-    res["accent"] = tsv_row["アクセント型"]
-    res["pos"] = tsv_row["品詞"]
-    res["bungo-type"] = tsv_row["文語などの\n種別"]
-    res["amendment"] = tsv_row["補足"]
-    keys = ['意味 1.', '意味 2.', '意味 3.', '意味 4.', '意味 5.']
-    res["meaning"] = [tsv_row[key] for key in keys]
-    res["remarks"] = tsv_row["備考"]
-    return res
+class Oki2YamatoConverter():
+    source = "./resources/base_lists/okinawa_01.tsv"
+
+    def convert(tsv_row):
+        res = {}
+        res["page-in-dict"] = tsv_row["辞書\nページ"]
+        pronuc = tsv_row["見出し語"]
+        res["pronunciation"] = pronuc
+        res["index"] = convert2kana(pronuc)
+        res["accent"] = tsv_row["アクセント型"]
+        res["pos"] = tsv_row["品詞"]
+        res["bungo-type"] = tsv_row["文語などの\n種別"]
+        res["amendment"] = tsv_row["補足"]
+        keys = ['意味 1.', '意味 2.', '意味 3.', '意味 4.', '意味 5.']
+        res["meaning"] = [tsv_row[key] for key in keys]
+        res["remarks"] = tsv_row["備考"]
+        return res
+
+
+class Yamato2OkiConverter():
+    source = "./resources/base_lists/okinawa_02.tsv"
+
+    def convert(tsv_row):
+        res = {}
+        res["page-in-dict"] = tsv_row["辞書\nページ"]
+        res["index"] = tsv_row["見出し"]
+        res["kanji"] = tsv_row["見出しの漢字"]
+        res["explanation"] = tsv_row["見出しの説明"]
+        res["meaning"] = tsv_row["内容"]
+        return res
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dict_type',
+                        choices=['o2y', 'y2o'],
+                        help="o2y:沖日辞典(resources/base_lists/okinawa_01.tsv からjsonへ変換)。\ny2o: 日沖辞典(resources/base_lists/okinawa_02.tsv からjsonへ変換)")
+    return parser.parse_args()
+
+
+converter_dict = {"o2y": Oki2YamatoConverter, "y2o": Yamato2OkiConverter}
 
 
 def main():
+    args = parse_args()
+    converter = converter_dict[args.dict_type]
     entry_list = []
-    with open("./resources/base_lists/okinawa_01.tsv", 'r') as base_file:
+    with open(converter.source, 'r') as base_file:
         base_tsv = DictReader(base_file, delimiter='\t')
 
         for i, entry in enumerate(base_tsv):
-            new_entry = convert(entry)
             new_entry = {"id": i}
-            new_entry.update(convert(entry))
+            new_entry.update(converter.convert(entry))
             entry_list.append(new_entry)
-    with open("./resources/base_lists/okinawa_01.json", 'w') as base_json:
+    new_path = converter.source.replace(".tsv", ".json")
+    with open(new_path, 'w') as base_json:
         json.dump(entry_list, base_json, ensure_ascii=False)
 
 
--- scripts/xlsx2tsv.py
+++ scripts/xlsx2tsv.py
@@ -2,18 +2,31 @@
 国立国語研究所の沖縄語辞典のxlsx化されたデータを読み込んで、tsvファイルに変換します。
 """
 
+import argparse
 from csv import writer
+from pathlib import Path
 
 from openpyxl import load_workbook
 
-XLSX_OKI2YAMATO = "./data/okinawa_01.xlsx"
+path_dict = {"o2y": "./resources/okinawa_01.xlsx", "y2o": "./resources/okinawa_02.xlsx"}
 
 zen2han_dict = {chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}
 zen2han_dict.update({'、': ','})
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',
+                        choices=['o2y', 'y2o'],
+                        help="o2y:沖日辞典(okinawa_01.xlsx)。\ny2o: 日沖辞典(okinawa_02.xlsx)")
+    return parser.parse_args()
+
+
 def main():
-    workbook = load_workbook(XLSX_OKI2YAMATO)
+    args = parse_args()
+    target_filename = path_dict[args.filename]
+    print(target_filename)
+    workbook = load_workbook(target_filename)
 
     def cells2values(row):
         """xlsx形式の行を文字列の行に変換します。"""
@@ -22,8 +35,10 @@ def main():
         return new_row
 
     rows = [cells2values(row) for row in list(workbook.worksheets[0].rows)]
-    file_name = XLSX_OKI2YAMATO.replace(".xlsx", ".tsv")
-    with open(file_name, 'w') as tsv_file:
+    new_filename = target_filename.replace(".xlsx", ".tsv")
+    if Path(new_filename).exists():
+        raise Exception(f"{new_filename}は既に存在します。上書きできません。")
+    with open(new_filename, 'w') as tsv_file:
         tsv_writer = writer(tsv_file, delimiter="\t")
         for row in rows:
             tsv_writer.writerow(row)
