303887826ba1d2dcf4721715b27b5b818d19b939
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2023-09-05 13:40:18 +0900

    WIP:カナテーブル

--- src/phonetics/generate_kana_table.py
+++ src/phonetics/generate_kana_table.py
@@ -2,8 +2,9 @@ from typing import Any, Dict, List
 import json
 from collections import defaultdict
 from pprint import pprint
+import re
 
-vowels = ["a", "i", "u", "e", "o"]
+vowels = r'[aiueo]'
 
 with open("resources/phonetics-table.json", 'r') as fp:
     phonetics_table = json.load(fp)
@@ -12,19 +13,20 @@ consonant2syllables_dict: Dict[str, List] = defaultdict(list)
 
 
 def remove_vowel(phoneme: str) -> str:
-    for v in vowels:
-        phoneme = phoneme.replace(v, "")
-    return phoneme
+    return re.sub(vowels, r'', phoneme)
 
 
 for phonetics_entry in phonetics_table:
     for syllable in phonetics_entry["roman"]:
         consonant = remove_vowel(syllable)
-        if len(consonant) == 2 and not (consonant[0] in ["n", "h", "'", "?"]
-                                        ) and consonant[1] in ["j", "w"]:
+        if re.match(r"[^snh'?][jw]", consonant):
             key = consonant[0]
         else:
-            key = consonant
+            m = re.match(r"[’']?([jw]?)", consonant)
+            if m and m.group():
+                key = m.groups()[0]
+            else:
+                key = consonant
         consonant2syllables_dict[key] += [phonetics_entry]
 
 for k, v in consonant2syllables_dict.items():
