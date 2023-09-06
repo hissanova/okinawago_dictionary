6112d538b9f510d60a4c6cd491dbe6dea723f809
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2023-09-06 18:54:00 +0900

    yamato2okiconverter refactored bit

--- src/generate_base_json.py
+++ src/generate_base_json.py
@@ -2,11 +2,10 @@ import argparse
 import json
 from typing import List
 from csv import DictReader
-from enum import Enum
 from pathlib import Path
 import re
 
-from wanakana import is_char_en_num, is_japanese, is_romaji, to_hiragana
+from wanakana import is_romaji, to_hiragana
 
 from utils import create_index2id_table
 from kanahyouki import generate_phonetics, SocialClass
@@ -24,13 +23,13 @@ unicode_ranges = {
 }
 
 total_uni_ranges = "".join([r for r in unicode_ranges.values()])
+# oki_dict meaning string のパース用regex
+okinawan_in_sentence_pattern = re.compile(r"([-～a-zA-Z?\s']+)")
+example_sentences_pattern = re.compile(r"([-～a-zA-Z?\s',]+\.)")
 
 
 class Oki2YamatoConverter():
     source = "./resources/base_lists/okinawa_01.tsv"
-    # oki_dict meaning string のパース用regex
-    okinawan_in_sentence_pattern = re.compile(r"([-～a-zA-Z?\s']+)")
-    example_sentences_pattern = re.compile(r"([-～a-zA-Z?\s',]+\.)")
 
     @classmethod
     def convert(cls, tsv_row):
@@ -55,6 +54,27 @@ class Oki2YamatoConverter():
         res["remarks"] = tsv_row["備考"]
         return res
 
+    @classmethod
+    def _parse_meaning_string(cls, sentence: str):
+        split_s = example_sentences_pattern.split(sentence)
+        main_body = {
+            "sentence": split_s[0],
+            "okinawago": okinawan_in_sentence_pattern.findall(split_s[0])
+        }
+        example_sentences = []
+        examples = split_s[1:]
+        if examples:
+            for i in range(0, len(examples), 2):
+                example_sentences.append({
+                    "okinawa": examples[i],
+                    "yamato": examples[i + 1]
+                })
+        return {"body": main_body, "examples": example_sentences}
+
+    @classmethod
+    def _convert_oki_sentence2kana(cls, sentence: str):
+        pass
+
 
 class Yamato2OkiConverter():
     source = "./resources/base_lists/okinawa_02.tsv"
@@ -70,19 +90,46 @@ class Yamato2OkiConverter():
         return res
 
     @classmethod
+    def _parse_contents(cls, content_obj):
+        print(content_obj)
+        contents_dict = {}
+        translations = content_obj.split('/')
+        base_translations = []
+        for pronunciation in translations.pop(0).strip(".").split('，'):
+            base_translations.append(cls._make_oki_item(pronunciation))
+
+        contents_dict["base"] = base_translations
+        contents_dict["related"] = []
+        for related_str in translations:
+            related_str = related_str.replace(' ', '')
+            related_phrases = cls._split_related_words_str(related_str)
+            print(related_phrases)
+            contents_dict["related"].append(
+                [cls._make_oki_item(phrase) for phrase in related_phrases])
+        return contents_dict
+
+    @classmethod
+    def _split_related_words_str(cls, related_str: str) -> List[str]:
+        okinawan_in_related_words = re.compile(
+            r"((?:\(\w+\))?→?[-a-zA-Z?\s']+)")
+        split_str = okinawan_in_related_words.split(related_str)
+        split_str = [s for s in split_str if re.match(r"[^，.]", s)]
+        return split_str
+
+    @classmethod
     def _make_oki_item(cls, item_symbols):
         item_symbols = item_symbols.replace(" ", "")
         vocabulary = {"reference": False}
+        oki_vocab = {
+            "lang": "Okinawa",
+        }
         if item_symbols.startswith("→"):
             item_symbols = item_symbols[1:]
             vocabulary["reference"] = True
         if is_romaji(item_symbols):
-            vocabulary.update({
-                "lang":
-                "Okinawa",
-                "phonetics":
-                generate_phonetics(item_symbols).to_dict()
-            })
+            vocabulary.update(
+                oki_vocab,
+                **{"phonetics": generate_phonetics(item_symbols).to_dict()})
         # 関連フレーズ: "(敬語|小児語|卑語|時刻|植物名)\w+" の形のもの
         elif m := re.match(r"\((\w+)\)([\w→'?-]+)", item_symbols):
             connotation, item_symbols = m.groups()
@@ -90,25 +137,21 @@ class Yamato2OkiConverter():
         elif m := re.match(r"([a-zA-Z'?-]+)\(([\w，' ]+)\)\(?([\w，' ]*)\)?",
                            item_symbols):
             m_groups = m.groups()
-            vocabulary.update({
-                "lang":
-                "Okinawa",
-                "phonetics":
-                generate_phonetics(m_groups[0]).to_dict()
-            })
+            vocabulary.update(
+                oki_vocab,
+                **{"phonetics": generate_phonetics(m_groups[0]).to_dict()})
             rest = m_groups[1:]
             for related in rest:
                 if related:
                     if m_ := re.match(r"(敬語)([\w'，]+)", related):
                         vocabulary.update({
-                            "related": {
-                                "lang":
-                                "Okinawa",
+                            "related":
+                            oki_vocab.update({
                                 "phonetics":
                                 generate_phonetics(m_.groups()[1]).to_dict(),
                                 "connotation":
                                 "敬語"
-                            }
+                            })
                         })
                     elif m_ := re.match(r"([\w']+)(の種類)([\w'，]+)", related):
                         m_groups = m_.groups()
@@ -131,92 +174,9 @@ class Yamato2OkiConverter():
         return vocabulary
 
     @classmethod
-    def _parse_contents(cls, content_obj):
-        contents_dict = {}
-        translations = content_obj.split('/')
-        base_translations = []
-        for pronunciation in translations.pop(0).strip(".").split('，'):
-            base_translations.append(cls._make_oki_item(pronunciation))
-
-        contents_dict["base"] = base_translations
-        contents_dict["related"] = []
-        for related_str in translations:
-            related_str = related_str.replace(' ', '')
-            related_phrases = cls._split_related_words_str(related_str)
-            related_phrases = cls._flatten_period(related_phrases)
-            contents_dict["related"].append(
-                [cls._make_oki_item(phrase) for phrase in related_phrases])
-        return contents_dict
-
-    @classmethod
-    def _split_related_words_str(cls, related_words: str) -> List[str]:
-        """
-        WIP:正規表現に書き換える
-        """
-        split_word = []
-        current_mode = LangMode.JAP
-        current_chunk = ""
-        while related_words:
-            ch = related_words[0]
-            char_mode = _get_lang_mode(ch)
-            if char_mode == LangMode.PARETH:
-                closing_pos = related_words.index(")") + 1
-                current_chunk += related_words[:closing_pos]
-                related_words = related_words[closing_pos:]
-                continue
-            if char_mode == LangMode.COMMA:
-                if current_mode == LangMode.JAP:
-                    current_chunk += ch
-                else:
-                    if current_chunk:
-                        split_word.append(current_chunk)
-                    current_chunk = ''
-            elif char_mode == LangMode.OTHERS:
-                if current_chunk:
-                    split_word.append(current_chunk)
-                current_chunk = ch
-            else:
-                if current_mode == char_mode:
-                    current_chunk += ch
-                else:
-                    if current_chunk:
-                        if current_chunk != "→":
-                            split_word.append(current_chunk)
-                            current_chunk = ch
-                        else:
-                            current_chunk += ch
-                    else:
-                        current_chunk = ch
-                    current_mode = char_mode
-            related_words = related_words[1:]
-        split_word.append(current_chunk)
-        return split_word
-
-    @classmethod
     def _flatten_period(cls, target_list):
         nested_list = [e.split('.') for e in target_list]
         return [e for sublist in nested_list for e in sublist]
-
-
-class LangMode(Enum):
-    JAP = "japanese"
-    ENG = "english"
-    COMMA = "comma"
-    PARETH = "parenthesis"
-    OTHERS = "others"
-
-
-def _get_lang_mode(ch: str) -> LangMode:
-    if ch == '(':
-        return LangMode.PARETH
-    elif ch == '，':
-        return LangMode.COMMA
-    elif is_japanese(ch) or is_char_en_num(ch):
-        return LangMode.JAP
-    elif is_romaji(ch):
-        return LangMode.ENG
-    else:
-        return LangMode.OTHERS
 
 
 def parse_args():
@@ -232,16 +192,8 @@ def parse_args():
 converter_dict = {"o2y": Oki2YamatoConverter, "y2o": Yamato2OkiConverter}
 
 
-def main():
-    args = parse_args()
-    converter = converter_dict[args.dict_type]
+def load_n_convert(converter):
     entry_list = []
-    target_dir = Path(__file__).parent / "okinawago_dictionary"
-    new_path = target_dir / Path(converter.source).name.replace(
-        ".tsv", ".json")
-    index_table_path = target_dir / Path(converter.source).name.replace(
-        ".tsv", "_index-table.json")
-
     with open(converter.source, 'r') as base_file:
         base_tsv = DictReader(base_file, delimiter='\t')
 
@@ -250,7 +202,20 @@ def main():
             # print(converter.convert(entry))
             new_entry.update(converter.convert(entry))
             entry_list.append(new_entry)
+    return entry_list
 
+
+def main():
+    args = parse_args()
+    converter = converter_dict[args.dict_type]
+
+    target_dir = Path(__file__).parent / "okinawago_dictionary"
+    new_path = target_dir / Path(converter.source).name.replace(
+        ".tsv", ".json")
+    index_table_path = target_dir / Path(converter.source).name.replace(
+        ".tsv", "_index-table.json")
+
+    entry_list = load_n_convert(converter)
     with open(new_path, 'w') as base_json:
         json.dump(entry_list, base_json, ensure_ascii=False)
 
